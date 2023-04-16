92133f9af24a1ff8e2d7858dd84413f6121977b3
abhishek1337chatterjee <abhishek1337chatterjee@gmail.com>
2023-04-16 19:21:41 +0530

    started working on project

---
+++ src/Components/test.jsx
@@ -0,0 +1,168 @@
+<div className="grid  md:grid-cols-2 gap-4">
+          <div
+            style={{ backgroundImage: `url(${Pear})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center px-4 mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span>
+                <p className="text-2xl font-bold pb-2 text-white tracking-wider">
+                  Pear Website
+                </p>
+                <p className="sm: text-[13.5px] md:text-sm lg:text-lg text-gray-400 pt-2">
+                  Pear is built with the reference website "apple.com". It is an
+                  ecommerce website for electronic products with an eye-catching
+                  UI where the users can buy various electronic items, with
+                  customizable design selections.
+                </p>
+
+                <div className="flex flex-row sm:mt-1 md:mt-2">
+                  <p className="md:text-xl text-orange-400">Teck Stack:-</p>
+                  <p className="md:text-lg text-black-500 ml-3">
+                    React.js | Firebase | Redux | Chakra UI
+                  </p>
+                </div>
+              </span>
+              <div className=" pt-2 text-center">
+                <a href="https://pear-website.netlify.app/" target="_blank">
+                  <button className="text-center rounded-lg px-2 py-1 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/himanku/Pear-website"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-2 py-1 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${Wearly})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center px-4 mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span>
+                <p className="text-2xl font-bold pb-2 text-white tracking-wider">
+                  Wearly Website
+                </p>
+                <p className="sm: text-[15.5px] md:text-lg lg:text-lg text-gray-400 pt-2">
+                  Wearly plc is a British online fashion and cosmetic retailer.
+                  The company was founded in 2000 in London, primarily aimed at
+                  young adults
+                </p>
+
+                <div className="flex flex-row sm:mt-1 md:mt-2">
+                  <p className="md:text-xl text-orange-400">Teck Stack:-</p>
+                  <p className="md:text-lg text-black-500 ml-3">
+                    React.js | Node.js | Express.js | MongoDB
+                  </p>
+                </div>
+              </span>
+              <div className=" pt-2 text-center">
+                <a href="https://wearly.vercel.app/" target="_blank">
+                  <button className="text-center rounded-lg px-2 py-1 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/santoshy1101/forlorn-cause-7661"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-2 py-1 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${Freshly})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center px-4 mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span>
+                <p className="text-2xl font-bold pb-2 text-white tracking-wider">
+                  Freshly Website
+                </p>
+                <p className="sm: text-[13.5px] md:text-sm lg:text-lg text-gray-400 pt-2">
+                  Freshly delivers gourmet ready-made meals, prepared and
+                  delivered at the door. It is dedicated to making healthy
+                  eating and achieving health and fitness goals easier than
+                  ever.
+                </p>
+
+                <div className="flex flex-row sm:mt-1 md:mt-2">
+                  <p className="md:text-xl text-orange-400">Teck Stack:-</p>
+                  <p className="md:text-lg text-black-500 ml-3">
+                    HTML | CSS | JavaScript
+                  </p>
+                </div>
+              </span>
+              <div className=" pt-2 text-center">
+                <a
+                  href="https://fluffy-croquembouche-14d5f2.netlify.app/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-2 py-1 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/abhishek1337chatterjee/freshly.com"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-2 py-1 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${Fire})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center px-4 mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span>
+                <p className="text-2xl font-bold pb-2 text-white tracking-wider">
+                  Fire Website
+                </p>
+                <p className="sm: text-[13.5px] md:text-sm lg:text-lg text-gray-400 pt-2">
+                  The fire calculator is a calculator that will return the
+                  yearly expenses (as of Today) , year expenses (as of
+                  Retirement) and the fire number is the amount that you need to
+                  become financially independent.
+                </p>
+
+                <div className="flex flex-row sm:mt-1 md:mt-2">
+                  <p className="md:text-xl text-orange-400">Teck Stack:-</p>
+                  <p className="md:text-lg text-black-500 ml-3">
+                    React.js | React-Redux | Vite | CSS
+                  </p>
+                </div>
+              </span>
+              <div className=" pt-2 text-center">
+                <a href="https://fire-calculator-zeta.vercel.app/" target="_blank">
+                  <button className="text-center rounded-lg px-2 py-1 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/abhishek1337chatterjee/Fire-Calculator" target="_blank">
+                  <button className="text-center rounded-lg px-2 py-1 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
