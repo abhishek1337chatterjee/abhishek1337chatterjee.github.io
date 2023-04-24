64457bdee82c0a454ded7079444652c1c3fc1c9f
abhishek1337chatterjee <abhishek1337chatterjee@gmail.com>
2023-04-24 16:28:49 +0530

    added download resume button on About Section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import Resume from "../Assets/Abhishek-Chatterjee-Resume.pdf";
+import { Button } from "@mui/material";
+import { FaDownload } from "react-icons/fa";
 export const About = () => {
   return (
     <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
@@ -15,7 +17,7 @@ export const About = () => {
           </div>
         </div>
         <div className="max-w-[1000px] w-full px-6">
-          <p className=" text-justify text-xl lg:text-4xl">
+          <p className=" text-justify text-xl mb-[50px] lg:text-4xl">
             I am a highly determined, passionate and an enthusiastic Full Stack
             Developer. I focus on writing clean, elegant and efficient codes. I
             love combining the worlds of logic , accessible, and user-friendly
@@ -25,6 +27,33 @@ export const About = () => {
             skills with the right organization.
           </p>
         </div>
+        <Button
+                variant="contained"
+                startIcon={<FaDownload size={20} />}
+                sx={{
+                  color: "white",
+                  backgroundColor: "orange",
+                  borderColor: "green",
+                  fontWeight: "bold",
+                  fontSize: "15px",
+                  fontFamily: "",
+                  ":hover": {
+                    bgcolor: "pink",
+                    color: "white",
+                  },
+                  width: "120px",
+                  padding: "10px 40px 10px 40px",
+                }}
+                onClick={() => {
+                  window.open(
+                    "https://drive.google.com/file/d/1GJ75qUzJh6KWhj95osLf77DGmUBoUNyz/view?usp=share_link"
+                  );
+                }}
+              >
+                <a href={Resume} download="fw20_1254-Abhishek-Chatterjee-Resume.pdf">
+                  Resume{" "}
+                </a>
+              </Button>
       </div>
     </div>
   );
